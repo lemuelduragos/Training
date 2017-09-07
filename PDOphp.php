9e0695061a1cab589fd5230e44d466427b7afe02
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-09-07 18:01:55 +0800

    modified

--- PDOphp.php
+++ PDOphp.php
@@ -26,19 +26,71 @@ function addRow($tableName, $value) {
     VALUES ('$value')";
     $conn = new DatabaseConnection();
     $conn->exec($sqlInsert);
-    echo "New record created successfully";
+    echo "New record created successfully<br>";
 
 }
 
 function selectAllRows($tableNameSelectAll) {
 	$sqlInsert = "SELECT* FROM $tableNameSelectAll";
 	$conn = new DatabaseConnection();
-	$stmt = $conn->prepare($sql);
+	$stmt = $conn->prepare($sqlInsert);
 	$stmt->execute();
 	$cou = $stmt->rowCount();
-	$result = $stmt->fetch();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo "<table>";
+	foreach($result as $row) {
+		echo "<tr><td>".$row['first_name']."</td>";
+		echo "<td>".$row['middle_name']."</td>";
+		echo "<td>".$row['last_name']."</td>";
+		echo "<td>".$row['gender']."</td>";
+		echo "<td>".$row['birthday']."</td>";
+		echo "<td>".$row['created_date']."</td>";
+		echo "<td>".$row['modified_date']."</td>";
+		echo "<td>".$row['created_ip']."</td>";
+		echo "<td>".$row['modified_ip']."</td></tr>";
+	}
+	echo "</table><br><br>";
 }
 
+function selectRow($tableNameSelectAll) {
+	$sqlInsert = "SELECT* FROM $tableNameSelectAll LIMIT 1";
+	$conn = new DatabaseConnection();
+	$stmt = $conn->prepare($sqlInsert);
+	$stmt->execute();
+	$cou = $stmt->rowCount();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo "<table>";
+	foreach($result as $row) {
+		echo "<tr><td>".$row['first_name']."</td>";
+		echo "<td>".$row['middle_name']."</td>";
+		echo "<td>".$row['last_name']."</td>";
+		echo "<td>".$row['gender']."</td>";
+		echo "<td>".$row['birthday']."</td>";
+		echo "<td>".$row['created_date']."</td>";
+		echo "<td>".$row['modified_date']."</td>";
+		echo "<td>".$row['created_ip']."</td>";
+		echo "<td>".$row['modified_ip']."</td></tr>";
+	}
+	echo "</table>";
+}
+
+
+
+function updateRow($columnName, $newValue, $id) {
+	$sqlUpdate = "UPDATE users SET $columnName='$newValue' WHERE id=$id";
+    $conn = new DatabaseConnection();
+    $stmt = $conn->prepare($sqlUpdate);
+    $stmt->execute();
+    echo "Record successfully updated!<br>";
+
+}
+
+function deleteRow($tableName, $id){
+	$sqlDelete = "DELETE FROM $tableName WHERE id= '$id'";
+	$conn = new DatabaseConnection();
+	$conn->exec($sqlDelete);
+	echo "Record Successfully Deleted!";
+}
 
 
 
@@ -52,7 +104,14 @@ createTable('round',
 );
 
 addRow('round', 'Lemuel');
+
+selectAllRows('users');
+selectRow('users');
+updateRow('first_name','firstname1', '1');
+deleteRow('users','50');
+
 ?>
 
 
   
+
